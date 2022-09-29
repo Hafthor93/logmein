0fd8889f43086bb0dfb91a0646b13810b3c1d1c5
Hafthor93 <101139553+Hafthor93@users.noreply.github.com>
2022-09-29 21:58:02 +0000

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "login-button": "#782143",
+      "white": "white",
+      "login-background": "#302E7B",
+      "log-container": "#F2F5F9",
+      "username-password": "#A9B3C4"
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
